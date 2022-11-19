502941b9d672c3f42e1e50b44bebd3d825567aa2
unknown <Christiandevega12@gmail.com>
2022-11-19 11:19:26 +0800

    fixed a bug

--- audiobook.py
+++ audiobook.py
@@ -2,7 +2,10 @@ import pyttsx3
 import PyPDF2
 
 pdf_book = open("Overlord_Vol1.pdf")
-pdf_reader=PyPDF2.PdfFileReader("pdf_book")
+pdf_reader=PyPDF2.PdfFileReader("Overlord_Vol1.pdf")
+content_pages=pdf_reader.numPages
+print(content_pages)
+
 Auto_reader = pyttsx3.init()
 Auto_reader.say("Hello, Everyone!")
 Auto_reader.runAndWait() 
