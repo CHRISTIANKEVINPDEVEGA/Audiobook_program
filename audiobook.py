94e3c1f82906712129387ae38dce1946e6239f9b
unknown <Christiandevega12@gmail.com>
2022-11-19 12:01:27 +0800

    user can now input the range of pages they wan to be read by the program

--- audiobook.py
+++ audiobook.py
@@ -7,9 +7,13 @@ pdf_book = open("Overlord_Vol1.pdf")
 pdf_reader=PyPDF2.PdfFileReader("Overlord_Vol1.pdf")
 content_pages=pdf_reader.numPages
 print(f"Number of Pages of the pdf book: {content_pages}")
+print("Enter the range of pages you want to be read")
+user_input=int(input("From page: "))
+user_input2=int(input("Until page(but not included): "))
 
-Auto_reader = pyttsx3.init()
-pdf_page=pdf_reader.getPage(10)
-text_page=pdf_page.extractText()
-Auto_reader.say(text_page)
-Auto_reader.runAndWait() 
+for each_page in range(user_input, user_input2):
+    Auto_reader = pyttsx3.init()
+    pdf_page=pdf_reader.getPage(each_page)
+    text_page=pdf_page.extractText()
+    Auto_reader.say(text_page)
+    Auto_reader.runAndWait() 
