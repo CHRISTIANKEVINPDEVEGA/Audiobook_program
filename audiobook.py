4e32c169e87d469077f1d0f10daa9a7ddf98679d
unknown <Christiandevega12@gmail.com>
2022-11-19 11:16:08 +0800

    installed the pyPDF2 to give program the ability to scan the content of the book

--- audiobook.py
+++ audiobook.py
@@ -1,5 +1,8 @@
 import pyttsx3
+import PyPDF2
 
+pdf_book = open("Overlord_Vol1.pdf")
+pdf_reader=PyPDF2.PdfFileReader("pdf_book")
 Auto_reader = pyttsx3.init()
 Auto_reader.say("Hello, Everyone!")
 Auto_reader.runAndWait() 
