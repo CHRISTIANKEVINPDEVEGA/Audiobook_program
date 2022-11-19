28017c8581f5a17f01c375faa6403dd5bc5e4ce3
unknown <Christiandevega12@gmail.com>
2022-11-19 11:37:39 +0800

    The program can now read the one specific page from the pdf book

--- audiobook.py
+++ audiobook.py
@@ -1,12 +1,15 @@
 import pyttsx3
 import PyPDF2
 
+print("Welcome to the Audio book program")
+
 pdf_book = open("Overlord_Vol1.pdf")
 pdf_reader=PyPDF2.PdfFileReader("Overlord_Vol1.pdf")
 content_pages=pdf_reader.numPages
-print(content_pages)
+print(f"Number of Pages of the pdf book: {content_pages}")
 
 Auto_reader = pyttsx3.init()
-Auto_reader.say("Hello, Everyone!")
+pdf_page=pdf_reader.getPage(10)
+text_page=pdf_page.extractText()
+Auto_reader.say(text_page)
 Auto_reader.runAndWait() 
-print("Audio book")
